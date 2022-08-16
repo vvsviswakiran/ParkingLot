7407516516fc51d52fe7fe43aa7ffb5bfd93e568
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-16 23:08:07 +0530

    Add Ticket tests and definition

--- lib/ticket.go
+++ lib/ticket.go
@@ -5,11 +5,12 @@ type Ticket struct {
 	CarParked  *Car
 }
 
-func CreateTicket(slotNo int, car *Car) {
+func CreateTicket(slotNo int, car *Car) Ticket {
 	if slotNo < 1 {
 		panic("Slot number must be positive")
 	}
 	if car == nil {
 		panic("Invalid car object")
 	}
+	return Ticket{slotNo, car}
 }
