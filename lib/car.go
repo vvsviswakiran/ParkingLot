c029d84db9d44d299d3b8f19a83022826a22ae3f
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-16 22:33:30 +0530

    Add Car tests and definition

--- lib/car.go
+++ lib/car.go
@@ -5,11 +5,12 @@ type Car struct {
 	Color              string
 }
 
-func CreateCar(regNo, color string) {
+func CreateCar(regNo, color string) Car {
 	if color == "" {
 		panic("Color cannot be an empty string")
 	}
 	if len(regNo) != 10 {
 		panic("Length of registration number must be 10")
 	}
+	return Car{regNo, color}
 }
