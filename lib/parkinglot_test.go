20e147f8d7dc389eca96e5d9ccf2c8ac3161b29b
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-17 07:44:27 +0530

    Add Fail - CreateParkingLot test - should not panic when creating a parking lot

---
+++ lib/parkinglot_test.go
@@ -0,0 +1,14 @@
+package parkinglot
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateParkingLot(t *testing.T) {
+	t.Run("should not panic when creating a parking lot object", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			CreateParkingLot(12)
+		})
+	})
+}
