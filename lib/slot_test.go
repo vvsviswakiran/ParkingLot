f62b339a58ee6eba81faa07e53fb043818b72920
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-17 07:39:13 +0530

    Add Fail - CreateSlot test - should not panic when slot is created with nil as car object

--- lib/slot_test.go
+++ lib/slot_test.go
@@ -7,14 +7,22 @@ import (
 
 func TestCreateSlot(t *testing.T) {
 	t.Run("should not panic when CreateSlot is called", func(t *testing.T) {
+		car := CreateCar("AP90GH2345", "White")
 		assert.NotPanics(t, func() {
-			CreateSlot(123, true)
+			CreateSlot(123, false, &car)
 		})
 	})
 
 	t.Run("should panic when slot number passed is less than 1", func(t *testing.T) {
+		car := CreateCar("AP90GH2345", "White")
 		assert.Panics(t, func() {
-			CreateSlot(-1, true)
+			CreateSlot(-1, false, &car)
+		})
+	})
+
+	t.Run("should not panic when slot is created with no car parked", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			CreateSlot(23, true, nil)
 		})
 	})
 }
