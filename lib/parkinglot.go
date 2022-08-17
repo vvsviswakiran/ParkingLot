63267c4c6679f98a6b65b03edc184db95bc4222d
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-17 09:05:47 +0530

    Add Pass - CreateParkingLot test - should panic when number of slots is negative

--- lib/parkinglot.go
+++ lib/parkinglot.go
@@ -6,6 +6,9 @@ type ParkingLot struct {
 }
 
 func CreateParkingLot(TotalSlots int) ParkingLot {
+	if TotalSlots < 0 {
+		panic("Slots must be positive")
+	}
 	Slots := make([]Slot, TotalSlots)
 	SlotNumber := 1
 	for i := range Slots {
