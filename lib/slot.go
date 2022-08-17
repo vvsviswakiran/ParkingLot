710d46a9295caa94f88a54d9aa9597f02e69daf6
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-17 07:40:06 +0530

    Add Pass - CreateSlot test - should not panic when slot is created with nil as car object

--- lib/slot.go
+++ lib/slot.go
@@ -3,11 +3,12 @@ package parkinglot
 type Slot struct {
 	SlotNumber   int
 	Availability bool
+	CarParked    *Car
 }
 
-func CreateSlot(slotNo int, availability bool) Slot {
+func CreateSlot(slotNo int, availability bool, car *Car) Slot {
 	if slotNo < 1 {
 		panic("Slot number must be positive")
 	}
-	return Slot{slotNo, availability}
+	return Slot{slotNo, availability, car}
 }
