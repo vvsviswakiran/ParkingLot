ea99c161c846aa107a5f994fd9ac9fa33279c1a5
vvsviswakiran <vvsai.viswa@shopup.org>
2022-08-17 16:27:34 +0530

    Final Commit

--- lib/parkinglot_test.go
+++ lib/parkinglot_test.go
@@ -99,19 +99,17 @@ func TestDeleteTicketAndFreeSlot(t *testing.T) {
 }
 
 func TestRegistrationNumbersOfCarsWithGivenColor(t *testing.T) {
+	parkingLot := CreateParkingLot(1)
+	car := CreateCar("AP90GH2345", "White")
+	parkingLot.AddCarAndIssueTicket(&car)
+
 	t.Run("should panic when there are no cars of given color", func(t *testing.T) {
-		parkingLot := CreateParkingLot(1)
-		car := CreateCar("AP90GH2345", "White")
-		parkingLot.AddCarAndIssueTicket(&car)
 		assert.Panics(t, func() {
 			RegistrationNumbersOfCarsWithGivenColor(parkingLot, "Green")
 		})
 	})
 
 	t.Run("should not panic when there are cars with given color", func(t *testing.T) {
-		parkingLot := CreateParkingLot(1)
-		car := CreateCar("AP90GH2345", "White")
-		parkingLot.AddCarAndIssueTicket(&car)
 		assert.NotPanics(t, func() {
 			RegistrationNumbersOfCarsWithGivenColor(parkingLot, "White")
 		})
